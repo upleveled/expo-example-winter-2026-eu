96dab20fa09da9e2e1ac8f6da5efe76c1819f7a9
Karl Horky <karl.horky@gmail.com>
2026-03-13 09:56:29 +0100

    Scaffold project

---
+++ app.config.ts
@@ -0,0 +1,56 @@
+import { type ExpoConfig } from 'expo/config';
+
+const config: ExpoConfig = {
+  name: 'expo-example-winter-2026-eu',
+  slug: 'expo-example-winter-2026-eu',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/images/icon.png',
+  scheme: 'expoexamplewinter2026eu',
+  userInterfaceStyle: 'automatic',
+  newArchEnabled: true,
+  ios: {
+    supportsTablet: true,
+  },
+  android: {
+    adaptiveIcon: {
+      backgroundColor: '#E6F4FE',
+      foregroundImage: './assets/images/android-icon-foreground.png',
+      backgroundImage: './assets/images/android-icon-background.png',
+      monochromeImage: './assets/images/android-icon-monochrome.png',
+    },
+    edgeToEdgeEnabled: true,
+    predictiveBackGestureEnabled: false,
+  },
+  web: {
+    output: 'static',
+    favicon: './assets/images/favicon.png',
+  },
+  plugins: [
+    'expo-router',
+    [
+      'expo-splash-screen',
+      {
+        image: './assets/images/splash-icon.png',
+        imageWidth: 200,
+        resizeMode: 'contain',
+        backgroundColor: '#ffffff',
+        dark: {
+          backgroundColor: '#000000',
+        },
+      },
+    ],
+  ],
+  experiments: {
+    typedRoutes: true,
+    reactCompiler: true,
+  },
+  extra: {
+    router: {},
+    eas: {
+      projectId: '6f6795a6-6542-43c5-ac7a-08f47205154c',
+    },
+  },
+};
+
+export default config;
---
+++ images.d.ts
@@ -0,0 +1,48 @@
+// Image types inspired by vite/client and Next.js type declarations
+//
+// TODO: Remove when Expo PR is merged:
+// - https://github.com/expo/expo/pull/43845
+declare module '*.png' {
+  const path: string;
+  export default path;
+}
+ 
+declare module '*.jpg' {
+  const path: string;
+  export default path;
+}
+ 
+declare module '*.jpeg' {
+  const path: string;
+  export default path;
+}
+ 
+declare module '*.webp' {
+  const path: string;
+  export default path;
+}
+ 
+declare module '*.avif' {
+  const path: string;
+  export default path;
+}
+ 
+declare module '*.gif' {
+  const path: string;
+  export default path;
+}
+ 
+declare module '*.ico' {
+  const path: string;
+  export default path;
+}
+ 
+declare module '*.bmp' {
+  const path: string;
+  export default path;
+}
+ 
+declare module '*.svg' {
+  const path: string;
+  export default path;
+}
